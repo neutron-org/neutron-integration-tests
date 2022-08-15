4cdd07c8578a53f90ba6831a512bc72bd0aa458c
sotnikov-s <ssotnikov93@gmail.com>
2022-08-15 22:36:59 +0300

    update neutron-contracts as remote dep #LSC-126

--- src/test_contracts/build_release.sh
+++ src/test_contracts/build_release.sh
@@ -1,16 +1,4 @@
-# put your path to the neutron-contracts packages folder
-export NEUTRON_PATH=~/projects/neutron-contracts/packages
-
 docker run --rm -v "$(pwd)":/code \
-    --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
-    --volume $NEUTRON_PATH:"/neutron-contracts/packages" \
-    --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-    cosmwasm/workspace-optimizer:0.12.6
-
-# TODO: once neutron-contracts code is opensourced, remove the above and uncomment
-# the original building command below
-# 
-# docker run --rm -v "$(pwd)":/code \
-#    --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
-#    --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-#    cosmwasm/workspace-optimizer:0.12.6
+   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+   cosmwasm/workspace-optimizer:0.12.6
