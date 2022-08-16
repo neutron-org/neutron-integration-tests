e42a4b7eac49e8e627d5d33729f85d908b7d5577
sotnikov-s <ssotnikov93@gmail.com>
2022-08-16 18:54:23 +0300

    add get recipient txs query test case

--- src/testcases/interchain_tx_query.test.ts
+++ src/testcases/interchain_tx_query.test.ts
@@ -32,7 +32,7 @@ describe('Neutron / Interchain TX Query', () => {
     });
   });
 
-  const watchedAddr: string = "neutron14xcrdjwwxtf9zr7dvaa97wy056se6r5erln9pf";
+  const watchedAddr: string = "neutron1fj6yqrkpw6fmp7f7jhj57dujfpwal4m25dafzx";
   describe("Register interchain query", () => {
     test("register transfers query", async () => {
       await registerTransfersQuery(
@@ -49,34 +49,36 @@ describe('Neutron / Interchain TX Query', () => {
       let queryResult = await getRegisteredQueryResult(cm, contractAddress, 1);
       expect(queryResult.registered_query.id).toEqual(1);
       expect(queryResult.registered_query.owner).toEqual(contractAddress);
-      // XXX: I could actually check that "key" is correctly derived from contractAddress,
-      //      but this requires bech32 decoding/encoding shenanigans
       expect(queryResult.registered_query.keys.length).toEqual(0);
       expect(queryResult.registered_query.query_type).toEqual("tx");
-      // XXX: think again, should it __really__ be empty?
       expect(queryResult.registered_query.transactions_filter).toEqual("{\"transfer.recipient\":\"" + watchedAddr + "\"}");
       expect(queryResult.registered_query.zone_id).toEqual(testState.sdk_2.chainID);
       expect(queryResult.registered_query.connection_id).toEqual(connectionId);
       expect(queryResult.registered_query.update_period).toEqual(5);
-      expect(queryResult.registered_query.last_emitted_height).toBeGreaterThan(0);
-
-      const previous_last_emitted_height = queryResult.registered_query.last_emitted_height;
-      // this should be enough for query results to be delivered at least once
-      await wait(BLOCK_TIME * 6);
-      queryResult = await getRegisteredQueryResult(cm, contractAddress, 1);
-      expect(queryResult.registered_query.last_emitted_height)
-        .toBeGreaterThan(previous_last_emitted_height);
     });
-  });
 
-  test("get recipient txs query", async () => {
-    const res = await cm2.msgSend(watchedAddr, '10000');
-    expect(res.length).toBeGreaterThan(0);
+    test("get recipient txs query", async () => {
+      let balances = await cm2.queryBalances(watchedAddr);
+      expect(balances.balances).toEqual([]);
+      const res = await cm2.msgSend(watchedAddr, '10000');
+      expect(res.length).toBeGreaterThan(0);
+      await wait(BLOCK_TIME);
+      balances = await cm2.queryBalances(watchedAddr);
+      expect(balances.balances).toEqual([
+        { amount: '10000', denom: 'stake' },
+      ]);
 
-    await wait(BLOCK_TIME * 6);
-    // returns nothing for now, to be investigated
-    let deposits = await queryRecipientTxs(cm, contractAddress, watchedAddr);
-    console.log(deposits);
+      await wait(BLOCK_TIME);
+      let deposits = await queryRecipientTxs(cm, contractAddress, watchedAddr);
+      expect(deposits.transfers).toEqual([
+        {
+          recipient: watchedAddr,
+          sender: cm2.wallet.address.toString(),
+          denom: "stake",
+          amount: "10000",
+        },
+      ]);
+    });
   });
 });
 
@@ -96,7 +98,7 @@ const registerTransfersQuery =
     );
     expect(txHash.length).toBeGreaterThan(0);
     const tx = await rest.tx.getTx(cm.sdk, txHash);
-    expect(tx?.data.tx_response.code).toEqual(0);
+    expect(tx?.data.tx_response?.code).toEqual(0);
 
     const attributes = getEventAttributesFromTx(tx?.data, "wasm", [
       "action", "connection_id", "zone_id", "query_type", "update_period"
