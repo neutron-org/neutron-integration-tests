bd1c972b9685e4d9993bd777a30383702424de27
swelf <golishkin@gmail.com>
2022-09-29 11:35:27 +0300

    enabled all tests

--- src/testcases/interchaintx.test.ts
+++ src/testcases/interchaintx.test.ts
@@ -210,281 +210,287 @@ describe('Neutron / Interchain TXs', () => {
       );
       expect(res2.data.delegation_responses).toEqual([]);
     });
-    // test('check acknowledgement success', async () => {
-    //   await waitWithAttempts(
-    //     cm1.sdk,
-    //     async () =>
-    //       (await cm1.queryContract<AcknowledgementResult>(contractAddress, {
-    //         acknowledgement_result: { interchain_account_id: icaId1 },
-    //       })) != null,
-    //   );
-    //   const res1 = await cm1.queryContractWithWait<AcknowledgementResult>(
-    //     contractAddress,
-    //     {
-    //       acknowledgement_result: { interchain_account_id: icaId1 },
-    //     },
-    //   );
-    //   expect(res1).toMatchObject<AcknowledgementResult>({
-    //     success: ['/cosmos.staking.v1beta1.MsgDelegate'],
-    //   });
+    test('check acknowledgement success', async () => {
+      await waitWithAttempts(
+        cm1.sdk,
+        async () =>
+          (await cm1.queryContract<AcknowledgementResult>(contractAddress, {
+            acknowledgement_result: { interchain_account_id: icaId1 },
+          })) != null,
+      );
+      const res1 = await cm1.queryContractWithWait<AcknowledgementResult>(
+        contractAddress,
+        {
+          acknowledgement_result: { interchain_account_id: icaId1 },
+        },
+      );
+      expect(res1).toMatchObject<AcknowledgementResult>({
+        success: ['/cosmos.staking.v1beta1.MsgDelegate'],
+      });
 
-    //   const res2 = await cm1.queryContract<AcknowledgementResult>(
-    //     contractAddress,
-    //     {
-    //       acknowledgement_result: { interchain_account_id: icaId2 },
-    //     },
-    //   );
-    //   expect(res2).toBe(null);
-    // });
+      const res2 = await cm1.queryContract<AcknowledgementResult>(
+        contractAddress,
+        {
+          acknowledgement_result: { interchain_account_id: icaId2 },
+        },
+      );
+      expect(res2).toBe(null);
+    });
 
-    // test('delegate for unknown validator from second ICA', async () => {
-    //   await cleanAckResults(cm1, contractAddress);
-    //   const res = await cm1.executeContract(
-    //     contractAddress,
-    //     JSON.stringify({
-    //       delegate: {
-    //         interchain_account_id: icaId2,
-    //         validator: 'nonexistent_address',
-    //         amount: '2000',
-    //       },
-    //     }),
-    //   );
-    //   expect(res.code).toEqual(0);
-    // });
-    // test('check acknowledgement error', async () => {
-    //   await waitWithAttempts(
-    //     cm1.sdk,
-    //     async () =>
-    //       (await cm1.queryContractWithWait<AcknowledgementResult>(
-    //         contractAddress,
-    //         {
-    //           acknowledgement_result: {
-    //             interchain_account_id: icaId2,
-    //           },
-    //         },
-    //       )) != null,
-    //   );
-    //   const res = await cm1.queryContractWithWait<AcknowledgementResult>(
-    //     contractAddress,
-    //     {
-    //       acknowledgement_result: {
-    //         interchain_account_id: icaId2,
-    //       },
-    //     },
-    //   );
-    //   expect(res).toMatchObject<AcknowledgementResult>({
-    //     error: [
-    //       'message',
-    //       'ABCI code: 1: error handling packet on host chain: see events for details',
-    //     ],
-    //   });
-    // });
+    test('delegate for unknown validator from second ICA', async () => {
+      await cleanAckResults(cm1, contractAddress);
+      const res = await cm1.executeContract(
+        contractAddress,
+        JSON.stringify({
+          delegate: {
+            interchain_account_id: icaId2,
+            validator: 'nonexistent_address',
+            amount: '2000',
+            denom: cm2.denom,
+          },
+        }),
+      );
+      expect(res.code).toEqual(0);
+    });
+    test('check acknowledgement error', async () => {
+      await waitWithAttempts(
+        cm1.sdk,
+        async () =>
+          (await cm1.queryContractWithWait<AcknowledgementResult>(
+            contractAddress,
+            {
+              acknowledgement_result: {
+                interchain_account_id: icaId2,
+              },
+            },
+          )) != null,
+      );
+      const res = await cm1.queryContractWithWait<AcknowledgementResult>(
+        contractAddress,
+        {
+          acknowledgement_result: {
+            interchain_account_id: icaId2,
+          },
+        },
+      );
+      expect(res).toMatchObject<AcknowledgementResult>({
+        error: [
+          'message',
+          'ABCI code: 1: error handling packet on host chain: see events for details',
+        ],
+      });
+    });
 
-    // test('undelegate from first ICA, delegate from second ICA', async () => {
-    //   await cleanAckResults(cm1, contractAddress);
-    //   const res1 = await cm1.executeContract(
-    //     contractAddress,
-    //     JSON.stringify({
-    //       undelegate: {
-    //         interchain_account_id: icaId1,
-    //         validator: testState.wallets.val2.address.toString(),
-    //         amount: '1000',
-    //       },
-    //     }),
-    //   );
-    //   expect(res1.code).toEqual(0);
+    test('undelegate from first ICA, delegate from second ICA', async () => {
+      await cleanAckResults(cm1, contractAddress);
+      const res1 = await cm1.executeContract(
+        contractAddress,
+        JSON.stringify({
+          undelegate: {
+            interchain_account_id: icaId1,
+            validator: testState.wallets.cosmos.val1.address.toString(),
+            amount: '1000',
+            denom: cm2.denom,
+          },
+        }),
+      );
+      expect(res1.code).toEqual(0);
 
-    //   const res2 = await cm1.executeContract(
-    //     contractAddress,
-    //     JSON.stringify({
-    //       delegate: {
-    //         interchain_account_id: icaId2,
-    //         validator: testState.wallets.val2.address.toString(),
-    //         amount: '2000',
-    //       },
-    //     }),
-    //   );
-    //   expect(res2.code).toEqual(0);
-    // });
+      const res2 = await cm1.executeContract(
+        contractAddress,
+        JSON.stringify({
+          delegate: {
+            interchain_account_id: icaId2,
+            validator: testState.wallets.cosmos.val1.address.toString(),
+            amount: '2000',
+            denom: cm2.denom,
+          },
+        }),
+      );
+      expect(res2.code).toEqual(0);
+    });
 
-    // test('check acknowledgements', async () => {
-    //   await waitWithAttempts(
-    //     cm1.sdk,
-    //     async () =>
-    //       (await cm1.queryContractWithWait<AcknowledgementResult>(
-    //         contractAddress,
-    //         {
-    //           acknowledgement_result: { interchain_account_id: icaId1 },
-    //         },
-    //       )) != null,
-    //   );
-    //   const res1 = await cm1.queryContractWithWait<AcknowledgementResult>(
-    //     contractAddress,
-    //     {
-    //       acknowledgement_result: { interchain_account_id: icaId1 },
-    //     },
-    //   );
-    //   expect(res1).toMatchObject<AcknowledgementResult>({
-    //     success: ['/cosmos.staking.v1beta1.MsgUndelegate'],
-    //   });
+    test('check acknowledgements', async () => {
+      await waitWithAttempts(
+        cm1.sdk,
+        async () =>
+          (await cm1.queryContractWithWait<AcknowledgementResult>(
+            contractAddress,
+            {
+              acknowledgement_result: { interchain_account_id: icaId1 },
+            },
+          )) != null,
+      );
+      const res1 = await cm1.queryContractWithWait<AcknowledgementResult>(
+        contractAddress,
+        {
+          acknowledgement_result: { interchain_account_id: icaId1 },
+        },
+      );
+      expect(res1).toMatchObject<AcknowledgementResult>({
+        success: ['/cosmos.staking.v1beta1.MsgUndelegate'],
+      });
 
-    //   await waitWithAttempts(
-    //     cm1.sdk,
-    //     async () =>
-    //       (await cm1.queryContractWithWait<AcknowledgementResult>(
-    //         contractAddress,
-    //         {
-    //           acknowledgement_result: { interchain_account_id: icaId2 },
-    //         },
-    //       )) != null,
-    //   );
-    //   const res2 = await cm1.queryContractWithWait<AcknowledgementResult>(
-    //     contractAddress,
-    //     {
-    //       acknowledgement_result: { interchain_account_id: icaId2 },
-    //     },
-    //   );
-    //   expect(res2).toMatchObject<AcknowledgementResult>({
-    //     success: ['/cosmos.staking.v1beta1.MsgDelegate'],
-    //   });
-    // });
-    // test('delegate with timeout', async () => {
-    //   await cleanAckResults(cm1, contractAddress);
-    //   const res = await cm1.executeContract(
-    //     contractAddress,
-    //     JSON.stringify({
-    //       delegate: {
-    //         interchain_account_id: icaId1,
-    //         validator: testState.wallets.val2.address.toString(),
-    //         amount: '10',
-    //         timeout: 1,
-    //       },
-    //     }),
-    //   );
-    //   expect(res.code).toEqual(0);
-    // });
-    // test('check acknowledgements after timeout', async () => {
-    //   await waitWithAttempts(
-    //     cm1.sdk,
-    //     async () =>
-    //       (await cm1.queryContractWithWait<AcknowledgementResult>(
-    //         contractAddress,
-    //         {
-    //           acknowledgement_result: { interchain_account_id: icaId1 },
-    //         },
-    //       )) != null,
-    //     50, // we need to wait quite a lot of time to make sure hermes started timeout logic
-    //   );
-    //   const res1 = await cm1.queryContractWithWait<AcknowledgementResult>(
-    //     contractAddress,
-    //     {
-    //       acknowledgement_result: { interchain_account_id: icaId1 },
-    //     },
-    //   );
-    //   expect(res1).toMatchObject<AcknowledgementResult>({
-    //     timeout: 'message',
-    //   });
-    // });
-    // test('delegate after the ICA channel was closed', async () => {
-    //   let rawLog: any;
-    //   try {
-    //     rawLog = (
-    //       await cm1.executeContract(
-    //         contractAddress,
-    //         JSON.stringify({
-    //           delegate: {
-    //             interchain_account_id: icaId1,
-    //             validator: testState.wallets.val2.address.toString(),
-    //             amount: '10',
-    //             timeout: 1,
-    //           },
-    //         }),
-    //       )
-    //     ).raw_log;
-    //   } catch (e) {
-    //     rawLog = e.message;
-    //   }
-    //   expect(rawLog.includes('no active channel for this owner'));
-    // });
-    // test('recreate ICA1', async () => {
-    //   const res = await cm1.executeContract(
-    //     contractAddress,
-    //     JSON.stringify({
-    //       register: {
-    //         connection_id: connectionId,
-    //         interchain_account_id: icaId1,
-    //       },
-    //     }),
-    //   );
-    //   expect(res.code).toEqual(0);
-    //   await waitWithAttempts(cm1.sdk, async () => {
-    //     const channels = await cm1.listIBCChannels();
-    //     // wait until there is one channel for ictx
-    //     // + 2 we have created previously
-    //     // + 1 we have created just now
-    //     return channels.channels.length == 4;
-    //   });
-    //   await waitWithAttempts(
-    //     cm1.sdk,
-    //     async () =>
-    //       (
-    //         await cm1.listIBCChannels()
-    //       ).channels.filter((c) => c.channel_id == 'channel-3')[0].state ==
-    //       'STATE_OPEN',
-    //   );
-    // });
-    // test('delegate from first ICA after ICA recreation', async () => {
-    //   await cleanAckResults(cm1, contractAddress);
-    //   const res = await cm1.executeContract(
-    //     contractAddress,
-    //     JSON.stringify({
-    //       delegate: {
-    //         interchain_account_id: icaId1,
-    //         validator: testState.wallets.val2.address.toString(),
-    //         amount: '20',
-    //       },
-    //     }),
-    //   );
-    //   expect(res.code).toEqual(0);
-    // });
-    // test('check acknowledgement success after ICA recreation', async () => {
-    //   await waitWithAttempts(
-    //     cm1.sdk,
-    //     async () =>
-    //       (await cm1.queryContractWithWait<AcknowledgementResult>(
-    //         contractAddress,
-    //         {
-    //           acknowledgement_result: { interchain_account_id: icaId1 },
-    //         },
-    //       )) != null,
-    //   );
-    //   const res1 = await cm1.queryContractWithWait<AcknowledgementResult>(
-    //     contractAddress,
-    //     {
-    //       acknowledgement_result: { interchain_account_id: icaId1 },
-    //     },
-    //   );
-    //   expect(res1).toMatchObject<AcknowledgementResult>({
-    //     success: ['/cosmos.staking.v1beta1.MsgDelegate'],
-    //   });
-    // });
-    // test('check validator state after ICA recreation', async () => {
-    //   const res1 = await rest.staking.delegatorDelegations(
-    //     cm2.sdk as CosmosSDK,
-    //     icaAddress1 as unknown as AccAddress,
-    //   );
-    //   expect(res1.data.delegation_responses).toEqual([
-    //     {
-    //       balance: { amount: '1020', denom: cm2.denom },
-    //       delegation: {
-    //         delegator_address: icaAddress1,
-    //         shares: '1020.000000000000000000',
-    //         validator_address:
-    //           'cosmosvaloper18hl5c9xn5dze2g50uaw0l2mr02ew57zk0auktn',
-    //       },
-    //     },
-    //   ]);
-    // });
+      await waitWithAttempts(
+        cm1.sdk,
+        async () =>
+          (await cm1.queryContractWithWait<AcknowledgementResult>(
+            contractAddress,
+            {
+              acknowledgement_result: { interchain_account_id: icaId2 },
+            },
+          )) != null,
+      );
+      const res2 = await cm1.queryContractWithWait<AcknowledgementResult>(
+        contractAddress,
+        {
+          acknowledgement_result: { interchain_account_id: icaId2 },
+        },
+      );
+      expect(res2).toMatchObject<AcknowledgementResult>({
+        success: ['/cosmos.staking.v1beta1.MsgDelegate'],
+      });
+    });
+    test('delegate with timeout', async () => {
+      await cleanAckResults(cm1, contractAddress);
+      const res = await cm1.executeContract(
+        contractAddress,
+        JSON.stringify({
+          delegate: {
+            interchain_account_id: icaId1,
+            validator: testState.wallets.cosmos.val1.address.toString(),
+            amount: '10',
+            denom: cm2.denom,
+            timeout: 1,
+          },
+        }),
+      );
+      expect(res.code).toEqual(0);
+    });
+    test('check acknowledgements after timeout', async () => {
+      await waitWithAttempts(
+        cm1.sdk,
+        async () =>
+          (await cm1.queryContractWithWait<AcknowledgementResult>(
+            contractAddress,
+            {
+              acknowledgement_result: { interchain_account_id: icaId1 },
+            },
+          )) != null,
+        50, // we need to wait quite a lot of time to make sure hermes started timeout logic
+      );
+      const res1 = await cm1.queryContractWithWait<AcknowledgementResult>(
+        contractAddress,
+        {
+          acknowledgement_result: { interchain_account_id: icaId1 },
+        },
+      );
+      expect(res1).toMatchObject<AcknowledgementResult>({
+        timeout: 'message',
+      });
+    });
+    test('delegate after the ICA channel was closed', async () => {
+      let rawLog: any;
+      try {
+        rawLog = (
+          await cm1.executeContract(
+            contractAddress,
+            JSON.stringify({
+              delegate: {
+                interchain_account_id: icaId1,
+                validator: testState.wallets.cosmos.val1.address.toString(),
+                amount: '10',
+                denom: cm2.denom,
+                timeout: 1,
+              },
+            }),
+          )
+        ).raw_log;
+      } catch (e) {
+        rawLog = e.message;
+      }
+      expect(rawLog.includes('no active channel for this owner'));
+    });
+    test('recreate ICA1', async () => {
+      const res = await cm1.executeContract(
+        contractAddress,
+        JSON.stringify({
+          register: {
+            connection_id: connectionId,
+            interchain_account_id: icaId1,
+          },
+        }),
+      );
+      expect(res.code).toEqual(0);
+      await waitWithAttempts(cm1.sdk, async () => {
+        const channels = await cm1.listIBCChannels();
+        // wait until there is one channel for ictx
+        // + 2 we have created previously
+        // + 1 we have created just now
+        return channels.channels.length == 4;
+      });
+      await waitWithAttempts(
+        cm1.sdk,
+        async () =>
+          (
+            await cm1.listIBCChannels()
+          ).channels.filter((c) => c.channel_id == 'channel-3')[0].state ==
+          'STATE_OPEN',
+      );
+    });
+    test('delegate from first ICA after ICA recreation', async () => {
+      await cleanAckResults(cm1, contractAddress);
+      const res = await cm1.executeContract(
+        contractAddress,
+        JSON.stringify({
+          delegate: {
+            interchain_account_id: icaId1,
+            validator: testState.wallets.cosmos.val1.address.toString(),
+            denom: cm2.denom,
+            amount: '20',
+          },
+        }),
+      );
+      expect(res.code).toEqual(0);
+    });
+    test('check acknowledgement success after ICA recreation', async () => {
+      await waitWithAttempts(
+        cm1.sdk,
+        async () =>
+          (await cm1.queryContractWithWait<AcknowledgementResult>(
+            contractAddress,
+            {
+              acknowledgement_result: { interchain_account_id: icaId1 },
+            },
+          )) != null,
+      );
+      const res1 = await cm1.queryContractWithWait<AcknowledgementResult>(
+        contractAddress,
+        {
+          acknowledgement_result: { interchain_account_id: icaId1 },
+        },
+      );
+      expect(res1).toMatchObject<AcknowledgementResult>({
+        success: ['/cosmos.staking.v1beta1.MsgDelegate'],
+      });
+    });
+    test('check validator state after ICA recreation', async () => {
+      const res1 = await rest.staking.delegatorDelegations(
+        cm2.sdk as CosmosSDK,
+        icaAddress1 as unknown as AccAddress,
+      );
+      expect(res1.data.delegation_responses).toEqual([
+        {
+          balance: { amount: '1020', denom: cm2.denom },
+          delegation: {
+            delegator_address: icaAddress1,
+            shares: '1020.000000000000000000',
+            validator_address:
+              'cosmosvaloper18hl5c9xn5dze2g50uaw0l2mr02ew57zk0auktn',
+          },
+        },
+      ]);
+    });
   });
 });
 
