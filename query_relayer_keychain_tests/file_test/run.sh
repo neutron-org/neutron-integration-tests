67c471c44c4deef678257aec6dc618017335fc53
oldremez <83083413+oldremez@users.noreply.github.com>
2022-10-31 17:16:36 +0300

    pass and file tests

---
+++ query_relayer_keychain_tests/file_test/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NEUTRON_BIN=/bin/neutrond
+KEYRING_PASS=rootroot
+SEED="record gift you once hip style during joke field prize dust unique length more pencil transfer quit train device arrive energy sort steak upset"
+ACCOUNT_NAME=os_demowallet
+
+echo "Inititalizing file keyring"
+
+echo -e "$SEED\n$KEYRING_PASS\n$KEYRING_PASS\n" | $NEUTRON_BIN keys add $ACCOUNT_NAME --recover --keyring-backend=file
+
+echo "file keyring initialized"
+
+export RELAYER_NEUTRON_CHAIN_SIGN_KEY_NAME=$ACCOUNT_NAME
+export RELAYER_NEUTRON_CHAIN_KEYRING_BACKEND=file
+export RELAYER_NEUTRON_CHAIN_KEYRING_PASSWORD=$KEYRING_PASS
+export RELAYER_NEUTRON_CHAIN_HOME_DIR=/root/.neutrond/
+
+./run-old.sh || true
+
+sleep infinity
