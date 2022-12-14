d6f9733654bb604e4519a34afadf9fe6d50227ef
swelf <golishkin@gmail.com>
2022-12-14 14:32:38 +0300

    linter fixes

--- src/testcases/interchain_tx_query_resubmit.test.ts
+++ src/testcases/interchain_tx_query_resubmit.test.ts
@@ -88,8 +88,8 @@ describe('Neutron / Interchain TX Query Resubmit', () => {
       expect(query.registered_query.query_type).toEqual('tx');
       expect(query.registered_query.transactions_filter).toEqual(
         '[{"field":"transfer.recipient","op":"Eq","value":"' +
-        watchedAddr1 +
-        '"}]',
+          watchedAddr1 +
+          '"}]',
       );
       expect(query.registered_query.connection_id).toEqual(connectionId);
       expect(query.registered_query.update_period).toEqual(query1UpdatePeriod);
