e7c6a5f32e390a19ee661c7d350d68762ecab8e4
sotnikov-s <ssotnikov93@gmail.com>
2023-01-13 12:13:53 +0300

    increase timeout for failures getWithAttempts

--- src/testcases/simple.test.ts
+++ src/testcases/simple.test.ts
@@ -370,6 +370,7 @@ describe('Neutron / Simple', () => {
           async () => cm.queryAckFailures(contractAddress),
           // Wait until there 4 failure in the list
           async (data) => data.failures.length == 4,
+          100,
         );
 
         expect(failuresAfterCall.failures).toEqual([
