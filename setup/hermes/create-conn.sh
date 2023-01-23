239071a595915cec5264358e44b18b4d5fc23e1d
oldremez <83083413+oldremez@users.noreply.github.com>
2023-01-23 16:31:01 +0200

    increasing hermes start stability

--- setup/hermes/create-conn.sh
+++ setup/hermes/create-conn.sh
@@ -4,6 +4,16 @@ set -e
 # Load shell variables
 . ./network/hermes/variables.sh
 
+echo "Waiting for a first neutron block..."
+while ! curl -f http://neutron-node:1317/blocks/1 >/dev/null 2>&1; do
+  sleep 1
+done
+
+echo "Waiting for a first gaia block..."
+while ! curl -f http://gaia-node:1317/blocks/1 >/dev/null 2>&1; do
+  sleep 1
+done
+
 ### Configure the clients and connection
 echo "Initiating connection handshake..."
 $HERMES_BINARY --config $CONFIG_DIR create connection --a-chain test-1 --b-chain test-2
