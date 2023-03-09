d18d9dcff5ebf386d97fc605ba2cceae6095fb06
Albert Andrejev <albert.andrejev@gmail.com>
2023-03-09 20:25:21 +0200

    Test credits vault voting power at different heights

--- src/testcases/credits.test.ts
+++ src/testcases/credits.test.ts
@@ -5,22 +5,20 @@ import { AccAddress, ValAddress } from '@cosmos-client/core/cjs/types';
 import { Wallet } from '../types';
 import { CreditsVaultConfig } from '../helpers/dao';
 import { NeutronContract } from '../helpers/types';
+import { InlineResponse20075TxResponse } from '@cosmos-client/core/cjs/openapi/api';
+import { getHeight } from '../helpers/wait';
 
 describe('Neutron / Credits', () => {
   let testState: TestStateLocalCosmosTestNet;
   let cm_dao: CosmosWrapper;
-  // let cm_owner: CosmosWrapper;
-  // let cm_manager: CosmosWrapper;
-  // let cm_holder: CosmosWrapper;
+  let cm_manager: CosmosWrapper;
   let dao_wallet: Wallet;
-  let owner_wallet: Wallet;
   let manager_wallet: Wallet;
   let airdrop_wallet: Wallet;
   let lockdrop_wallet: Wallet;
   let dao_addr: AccAddress | ValAddress;
   let airdrop_addr: AccAddress | ValAddress;
   let lockdrop_addr: AccAddress | ValAddress;
-  let owner_addr: AccAddress | ValAddress;
   let manager_addr: AccAddress | ValAddress;
 
   beforeAll(async () => {
@@ -29,12 +27,10 @@ describe('Neutron / Credits', () => {
     dao_wallet = testState.wallets.neutron.demo1;
     airdrop_wallet = testState.wallets.neutron.demo2;
     lockdrop_wallet = testState.wallets.neutron.icq;
-    owner_wallet = testState.wallets.neutron.rly1;
     manager_wallet = testState.wallets.neutron.rly1;
     dao_addr = dao_wallet.address;
     airdrop_addr = airdrop_wallet.address;
     lockdrop_addr = lockdrop_wallet.address;
-    owner_addr = owner_wallet.address;
     manager_addr = manager_wallet.address;
 
     cm_dao = new CosmosWrapper(
@@ -43,12 +39,12 @@ describe('Neutron / Credits', () => {
       dao_wallet,
       NEUTRON_DENOM,
     );
-    // cm_owner = new CosmosWrapper(
-    //   testState.sdk1,
-    //   testState.blockWaiter1,
-    //   dao_wallet,
-    //   NEUTRON_DENOM,
-    // );
+    cm_manager = new CosmosWrapper(
+      testState.sdk1,
+      testState.blockWaiter1,
+      manager_wallet,
+      NEUTRON_DENOM,
+    );
   });
 
   const original_description = 'A credtis vault for test purposes.';
@@ -69,7 +65,7 @@ describe('Neutron / Credits', () => {
         cm_dao,
         credits_contract_addr,
         original_description,
-        owner_addr.toString(),
+        dao_addr.toString(),
         manager_addr.toString(),
       );
     });
@@ -78,138 +74,196 @@ describe('Neutron / Credits', () => {
       expect(await getVaultConfig(cm_dao, credits_vault_addr)).toMatchObject({
         description: original_description,
         credits_contract_address: credits_contract_addr,
-        owner: owner_addr.toString(),
+        owner: dao_addr.toString(),
         manager: manager_addr.toString(),
       });
     });
 
-    // const new_description = 'A new description for the lockdrop vault.';
-    // test('Update config by manager: success', async () => {
-    //   const res = await updateLockdropVaultConfig(
-    //     cm_manager,
-    //     lockdrop_vault_addr,
-    //     owner_addr.toString(),
-    //     lockdrop_contract_addr.toString(),
-    //     manager_addr.toString(),
-    //     original_name,
-    //     new_description,
-    //   );
-    //   expect(res.code).toEqual(0);
+    const new_description = 'A new description for the lockdrop vault.';
+    test('Update config by manager: success', async () => {
+      const res = await updateVaultConfig(
+        cm_manager,
+        credits_vault_addr,
+        credits_contract_addr,
+        new_description,
+        dao_addr.toString(),
+        manager_addr.toString(),
+      );
+      expect(res.code).toEqual(0);
 
-    //   expect(
-    //     await getLockdropVaultConfig(cm_dao, lockdrop_vault_addr),
-    //   ).toMatchObject({
-    //     name: original_name,
-    //     description: new_description,
-    //     lockdrop_contract: lockdrop_contract_addr.toString(),
-    //     owner: owner_addr.toString(),
-    //     manager: manager_addr.toString(),
-    //   });
-    // });
+      expect(await getVaultConfig(cm_dao, credits_vault_addr)).toMatchObject({
+        description: new_description,
+        credits_contract_address: credits_contract_addr,
+        owner: dao_addr.toString(),
+        manager: manager_addr.toString(),
+      });
+    });
 
-    // test('Update config by manager: permission denied', async () => {
-    //   // change owner to manager
-    //   await expect(
-    //     updateLockdropVaultConfig(
-    //       cm_manager,
-    //       lockdrop_vault_addr,
-    //       manager_addr.toString(),
-    //       lockdrop_contract_addr.toString(),
-    //       manager_addr.toString(),
-    //       original_name,
-    //       original_description,
-    //     ),
-    //   ).rejects.toThrow(/Only owner can change owner/);
+    test('Update config by manager: permission denied', async () => {
+      // change owner to manager
+      await expect(
+        updateVaultConfig(
+          cm_manager,
+          credits_vault_addr,
+          credits_contract_addr,
+          original_description,
+          manager_addr.toString(),
+          manager_addr.toString(),
+        ),
+      ).rejects.toThrow(/Only owner can change owner/);
 
-    //   expect(
-    //     await getLockdropVaultConfig(cm_dao, lockdrop_vault_addr),
-    //   ).toMatchObject({
-    //     name: original_name,
-    //     description: new_description,
-    //     lockdrop_contract: lockdrop_contract_addr.toString(),
-    //     owner: owner_addr.toString(),
-    //     manager: manager_addr.toString(),
-    //   });
-    // });
+      expect(await getVaultConfig(cm_dao, credits_vault_addr)).toMatchObject({
+        description: new_description,
+        credits_contract_address: credits_contract_addr,
+        owner: dao_addr.toString(),
+        manager: manager_addr.toString(),
+      });
+    });
 
-    // test('Update config by owner', async () => {
-    //   // change owner to manager
-    //   let res = await updateLockdropVaultConfig(
-    //     cm_owner,
-    //     lockdrop_vault_addr,
-    //     manager_addr.toString(),
-    //     lockdrop_contract_addr.toString(),
-    //     manager_addr.toString(),
-    //     original_name,
-    //     original_description,
-    //   );
-    //   expect(res.code).toEqual(0);
+    test('Update config by owner', async () => {
+      // change owner to manager
+      let res = await updateVaultConfig(
+        cm_dao,
+        credits_vault_addr,
+        credits_contract_addr,
+        original_description,
+        manager_addr.toString(),
+        manager_addr.toString(),
+      );
+      expect(res.code).toEqual(0);
 
-    //   expect(
-    //     await getLockdropVaultConfig(cm_dao, lockdrop_vault_addr),
-    //   ).toMatchObject({
-    //     name: original_name,
-    //     description: original_description,
-    //     lockdrop_contract: lockdrop_contract_addr.toString(),
-    //     owner: manager_addr.toString(),
-    //     manager: manager_addr.toString(),
-    //   });
+      expect(await getVaultConfig(cm_dao, credits_vault_addr)).toMatchObject({
+        description: original_description,
+        credits_contract_address: credits_contract_addr,
+        owner: manager_addr.toString(),
+        manager: manager_addr.toString(),
+      });
 
-    //   // make sure new owner is promoted and get back to original lockdrop vault settings
-    //   res = await updateLockdropVaultConfig(
-    //     cm_manager,
-    //     lockdrop_vault_addr,
-    //     owner_addr.toString(),
-    //     lockdrop_contract_addr.toString(),
-    //     manager_addr.toString(),
-    //     original_name,
-    //     original_description,
-    //   );
-    //   expect(res.code).toEqual(0);
+      // make sure new owner is promoted and get back to original lockdrop vault settings
+      res = await updateVaultConfig(
+        cm_manager,
+        credits_vault_addr,
+        credits_contract_addr,
+        original_description,
+        dao_addr.toString(),
+        manager_addr.toString(),
+      );
+      expect(res.code).toEqual(0);
 
-    //   expect(
-    //     await getLockdropVaultConfig(cm_dao, lockdrop_vault_addr),
-    //   ).toMatchObject({
-    //     name: original_name,
-    //     description: original_description,
-    //     lockdrop_contract: lockdrop_contract_addr.toString(),
-    //     owner: owner_addr.toString(),
-    //     manager: manager_addr.toString(),
-    //   });
-    // });
+      expect(await getVaultConfig(cm_dao, credits_vault_addr)).toMatchObject({
+        description: original_description,
+        credits_contract_address: credits_contract_addr,
+        owner: dao_addr.toString(),
+        manager: manager_addr.toString(),
+      });
+    });
 
-    // test('Bonding and Unbonding', async () => {
-    //   await expect(
-    //     cm_holder.executeContract(
-    //       lockdrop_vault_addr,
-    //       JSON.stringify({
-    //         bond: {},
-    //       }),
-    //       [{ denom: NEUTRON_DENOM, amount: '1000' }],
-    //     ),
-    //   ).rejects.toThrow(/Bonding is not available for this contract/);
-    //   await expect(
-    //     cm_holder.executeContract(
-    //       lockdrop_vault_addr,
-    //       JSON.stringify({
-    //         unbond: {
-    //           amount: '1000',
-    //         },
-    //       }),
-    //     ),
-    //   ).rejects.toThrow(/Direct unbonding is not available for this contract/);
-    // });
+    test('Query total voting power at height', async () => {
+      const currentHeight = await getHeight(cm_manager.sdk);
+      expect(
+        await getTotalPowerAtHeight(
+          cm_manager,
+          credits_vault_addr,
+          currentHeight,
+        ),
+      ).toMatchObject({
+        height: currentHeight,
+        power: '0',
+      });
+    });
 
-    // test('Bonding status', async () => {
-    //   const status = await getVaultBondingStatus(
-    //     cm_holder,
-    //     lockdrop_vault_addr,
-    //     holder_addr.toString(),
-    //   );
-    //   expect(status.bonding_enabled).toEqual(false);
-    //   expect(status.unbondable_abount).toEqual('0');
-    //   expect(status.height).toBeGreaterThan(0);
-    // });
+    test('Query airdrop address voting power at height', async () => {
+      const currentHeight = await getHeight(cm_manager.sdk);
+      expect(
+        await getVotingPowerAtHeight(
+          cm_manager,
+          credits_vault_addr,
+          airdrop_addr.toString(),
+          currentHeight,
+        ),
+      ).toMatchObject({
+        height: currentHeight,
+        power: '0',
+      });
+    });
+
+    test('Query voting power at different heights', async () => {
+      const firstHeight = await getHeight(cm_manager.sdk);
+
+      await mintTokens(cm_dao, credits_contract_addr, '1000');
+      await cm_dao.blockWaiter.waitBlocks(1);
+      const secondHeight = await getHeight(cm_manager.sdk);
+
+      await mintTokens(cm_dao, credits_contract_addr, '1000');
+      await cm_dao.blockWaiter.waitBlocks(1);
+      const thirdHeight = await getHeight(cm_manager.sdk);
+
+      expect(
+        await getTotalPowerAtHeight(
+          cm_manager,
+          credits_vault_addr,
+          secondHeight,
+        ),
+      ).toMatchObject({
+        height: secondHeight,
+        power: '1000',
+      });
+      expect(
+        await getVotingPowerAtHeight(
+          cm_manager,
+          credits_vault_addr,
+          airdrop_addr.toString(),
+          secondHeight,
+        ),
+      ).toMatchObject({
+        height: secondHeight,
+        power: '1000',
+      });
+
+      expect(
+        await getTotalPowerAtHeight(
+          cm_manager,
+          credits_vault_addr,
+          firstHeight,
+        ),
+      ).toMatchObject({
+        height: firstHeight,
+        power: '0',
+      });
+      expect(
+        await getVotingPowerAtHeight(
+          cm_manager,
+          credits_vault_addr,
+          airdrop_addr.toString(),
+          firstHeight,
+        ),
+      ).toMatchObject({
+        height: firstHeight,
+        power: '0',
+      });
+
+      expect(
+        await getTotalPowerAtHeight(
+          cm_manager,
+          credits_vault_addr,
+          thirdHeight,
+        ),
+      ).toMatchObject({
+        height: thirdHeight,
+        power: '2000',
+      });
+      expect(
+        await getVotingPowerAtHeight(
+          cm_manager,
+          credits_vault_addr,
+          airdrop_addr.toString(),
+          thirdHeight,
+        ),
+      ).toMatchObject({
+        height: thirdHeight,
+        power: '2000',
+      });
+    });
   });
 });
 
@@ -247,14 +301,6 @@ const setupCreditsContract = async (
   when_withdrawable: string,
 ) => {
   const codeId = await cm.storeWasm(NeutronContract.CREDITS);
-  console.log(
-    JSON.stringify({
-      dao_address,
-      airdrop_address,
-      lockdrop_address,
-      when_withdrawable,
-    }),
-  );
   return (
     await cm.instantiate(
       codeId,
@@ -271,40 +317,74 @@ const setupCreditsContract = async (
 
 const getVaultConfig = async (
   cm: CosmosWrapper,
-  lockdrop_vault_contract: string,
+  credits_vault_contract: string,
 ): Promise<CreditsVaultConfig> =>
-  cm.queryContract<CreditsVaultConfig>(lockdrop_vault_contract, {
+  cm.queryContract<CreditsVaultConfig>(credits_vault_contract, {
     get_config: {},
   });
 
-// const updateVaultConfig = async (
-//   cm: CosmosWrapper,
-//   vault_contract: string,
-//   credits_contract_address: string,
-//   description: string,
-//   owner?: string,
-//   manager?: string,
-// ): Promise<InlineResponse20075TxResponse> =>
-//   cm.executeContract(
-//     vault_contract,
-//     JSON.stringify({
-//       update_config: {
-//         credits_contract_address: {
-//           address: {
-//             addr: credits_contract_address,
-//           },
-//         },
-//         description,
-//         owner: {
-//           address: {
-//             addr: owner,
-//           },
-//         },
-//         manager: {
-//           address: {
-//             addr: manager,
-//           },
-//         },
-//       },
-//     }),
-//   );
+const getTotalPowerAtHeight = async (
+  cm: CosmosWrapper,
+  credits_vault_contract: string,
+  height: number,
+): Promise<CreditsVaultConfig> =>
+  cm.queryContract<CreditsVaultConfig>(credits_vault_contract, {
+    total_power_at_height: {
+      height,
+    },
+  });
+
+const getVotingPowerAtHeight = async (
+  cm: CosmosWrapper,
+  credits_vault_contract: string,
+  address: string,
+  height: number,
+): Promise<CreditsVaultConfig> =>
+  cm.queryContract<CreditsVaultConfig>(credits_vault_contract, {
+    voting_power_at_height: {
+      address,
+      height,
+    },
+  });
+
+const mintTokens = async (
+  cm: CosmosWrapper,
+  credits_contract_address: string,
+  amount: string,
+): Promise<InlineResponse20075TxResponse> =>
+  cm.executeContract(
+    credits_contract_address,
+    JSON.stringify({
+      mint: {},
+    }),
+    [
+      {
+        amount,
+        denom: NEUTRON_DENOM,
+      },
+    ],
+  );
+
+const updateVaultConfig = async (
+  cm: CosmosWrapper,
+  vault_contract: string,
+  credits_contract_address: string,
+  description: string,
+  owner?: string,
+  manager?: string,
+): Promise<InlineResponse20075TxResponse> =>
+  cm.executeContract(
+    vault_contract,
+    JSON.stringify({
+      update_config: {
+        credits_contract_address,
+        description,
+        owner: {
+          address: {
+            addr: owner,
+          },
+        },
+        manager,
+      },
+    }),
+  );
