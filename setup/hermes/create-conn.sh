54097e5e2e551932f4c3663775a1e0142830d2e4
sotnikov-s <ssotnikov93@gmail.com>
2024-01-11 15:56:45 +0300

    add short-living IBC client creation

--- setup/hermes/create-conn.sh
+++ setup/hermes/create-conn.sh
@@ -10,4 +10,10 @@ while ! $HERMES_BINARY --config $CONFIG_DIR create connection --a-chain test-1 -
   sleep 1
 done
 
-sleep 2
+# a short living client is needed for client update proposal test
+echo "Initiating short living client..."
+while ! $HERMES_BINARY --config $CONFIG_DIR create client --trusting-period 5s --host-chain test-1 --reference-chain test-2; do
+  sleep 1
+done
+# wait until the short living client expires
+sleep 5
