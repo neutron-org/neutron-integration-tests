f07731f9185be313beff280f73c44935a35ee692
Albert Andrejev <albert.andrejev@gmail.com>
2024-02-07 20:24:23 +0200

    Add proposal votes tests

--- src/helpers/gaia.ts
+++ src/helpers/gaia.ts
@@ -4,6 +4,12 @@ import {
   MsgUndelegate,
 } from '@neutron-org/neutronjsplus/dist/proto/cosmos_sdk/cosmos/staking/v1beta1/tx_pb';
 import {
+  MsgSubmitProposal,
+  MsgVote,
+  TextProposal,
+  VoteOption,
+} from '@neutron-org/neutronjsplus/dist/proto/cosmos_sdk/cosmos/gov/v1beta1/tx_pb';
+import {
   packAnyMsg,
   WalletWrapper,
 } from '@neutron-org/neutronjsplus/dist/cosmos';
@@ -48,5 +54,59 @@ export const msgUndelegate = async (
     },
     [packAnyMsg('/cosmos.staking.v1beta1.MsgUndelegate', msgUndelegate)],
   );
+
+  return res?.tx_response;
+};
+
+export const msgSubmitProposal = async (
+  wallet: WalletWrapper,
+  proposer: string,
+  amount = '0',
+): Promise<BroadcastTx200ResponseTxResponse> => {
+  const msgSubmitProposal = new MsgSubmitProposal({
+    proposer,
+    content: {
+      typeUrl: '/cosmos.gov.v1beta1.TextProposal',
+      value: new TextProposal({
+        title: 'mock',
+        description: 'mock',
+      }).toBinary(),
+    },
+    initialDeposit: [{ denom: wallet.chain.denom, amount: '10000000' }],
+  });
+  const res = await wallet.execTx(
+    {
+      gas_limit: Long.fromString('500000'),
+      amount: [{ denom: wallet.chain.denom, amount: amount }],
+    },
+    [packAnyMsg('/cosmos.gov.v1beta1.MsgSubmitProposal', msgSubmitProposal)],
+  );
+
+  return res?.tx_response;
+};
+
+export const msgVote = async (
+  wallet: WalletWrapper,
+  voter: string,
+  proposalId: number,
+  amount = '0',
+): Promise<BroadcastTx200ResponseTxResponse> => {
+  console.log('msgVote', voter, proposalId, amount);
+  const msgVote = new MsgVote({
+    voter,
+    proposalId: BigInt(proposalId),
+    option: VoteOption.YES,
+  });
+
+  const res = await wallet.execTx(
+    {
+      gas_limit: Long.fromString('500000'),
+      amount: [{ denom: wallet.chain.denom, amount: amount }],
+    },
+    [packAnyMsg('/cosmos.gov.v1beta1.MsgVote', msgVote)],
+  );
+
+  console.log(res);
+
   return res?.tx_response;
 };
