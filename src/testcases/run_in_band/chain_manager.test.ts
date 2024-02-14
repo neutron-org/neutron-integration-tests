c6a6a4749dc9fdb75b1bd125397748acd8416f8c
Andrei Zavgorodnii <andrei.z@hadronlabs.org>
2024-02-14 14:49:37 +0200

    wip

--- src/testcases/run_in_band/chain_manager.test.ts
+++ src/testcases/run_in_band/chain_manager.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import '@neutron-org/neutronjsplus';
 import {
   WalletWrapper,
@@ -5,125 +6,107 @@ import {
   NEUTRON_DENOM,
 } from '@neutron-org/neutronjsplus/dist/cosmos';
 import { TestStateLocalCosmosTestNet } from '@neutron-org/neutronjsplus';
-import { getWithAttempts } from '@neutron-org/neutronjsplus/dist/wait';
 import {
   Dao,
   DaoMember,
-  getDaoContracts,
+  deployNeutronDao,
+  setupSubDaoTimelockSet,
 } from '@neutron-org/neutronjsplus/dist/dao';
-import { updateCronParamsProposal } from '@neutron-org/neutronjsplus/dist/proposal';
+import { Wallet } from '@neutron-org/neutronjsplus/dist/types';
+import cosmosclient from '@cosmos-client/core';
+import { waitSeconds } from '@neutron-org/neutronjsplus/dist/wait';
+import {
+  paramChangeProposal,
+  sendProposal,
+} from '@neutron-org/neutronjsplus/dist/proposal';
 
 import config from '../../config.json';
 
-describe('Neutron / Chain Manager', () => {
+describe('Neutron / Subdao', () => {
   let testState: TestStateLocalCosmosTestNet;
   let neutronChain: CosmosWrapper;
-  let neutronAccount: WalletWrapper;
-  let daoMember1: DaoMember;
-  let dao: Dao;
+  let neutronAccount1: WalletWrapper;
+  let neutronAccount2: WalletWrapper;
+  let subdaoMember1: DaoMember;
+  let mainDaoMember: DaoMember;
+  let demo1Wallet: Wallet;
+  let securityDaoWallet: Wallet;
+  let demo2Wallet: Wallet;
+  let securityDaoAddr: cosmosclient.AccAddress | cosmosclient.ValAddress;
+  let subDao: Dao;
+  let mainDao: Dao;
 
   beforeAll(async () => {
     testState = new TestStateLocalCosmosTestNet(config);
     await testState.init();
+    demo1Wallet = testState.wallets.qaNeutron.genQaWal1;
+    securityDaoWallet = testState.wallets.qaNeutronThree.genQaWal1;
+    demo2Wallet = testState.wallets.qaNeutronFour.genQaWal1;
+    securityDaoAddr = securityDaoWallet.address;
     neutronChain = new CosmosWrapper(
       testState.sdk1,
       testState.blockWaiter1,
       NEUTRON_DENOM,
     );
-    neutronAccount = new WalletWrapper(
-      neutronChain,
-      testState.wallets.qaNeutron.genQaWal1,
+    neutronAccount1 = new WalletWrapper(neutronChain, demo1Wallet);
+    neutronAccount2 = new WalletWrapper(neutronChain, demo2Wallet);
+
+    const daoContracts = await deployNeutronDao(neutronAccount1);
+    mainDao = new Dao(neutronChain, daoContracts);
+    mainDaoMember = new DaoMember(neutronAccount1, mainDao);
+    await mainDaoMember.bondFunds('10000');
+
+    subDao = await setupSubDaoTimelockSet(
+      neutronAccount1,
+      mainDao.contracts.core.address,
+      securityDaoAddr.toString(),
+      true,
     );
-    const daoCoreAddress = await neutronChain.getNeutronDAOCore();
-    const daoContracts = await getDaoContracts(neutronChain, daoCoreAddress);
 
-    dao = new Dao(neutronChain, daoContracts);
-    daoMember1 = new DaoMember(neutronAccount, dao);
+    subdaoMember1 = new DaoMember(neutronAccount1, subDao);
+
+    const subDaosList = await mainDao.getSubDaoList();
+    expect(subDaosList).toContain(subDao.contracts.core.address);
+
+    const votingPower = await subdaoMember1.queryVotingPower();
+    expect(votingPower.power).toEqual('1');
   });
 
-  describe('prepare: bond funds', () => {
-    test('bond form wallet 1', async () => {
-      await daoMember1.bondFunds('10000');
-      await getWithAttempts(
-        neutronChain.blockWaiter,
-        async () =>
-          await dao.queryVotingPower(daoMember1.user.wallet.address.toString()),
-        async (response) => response.power == 10000,
-        20,
-      );
-    });
-    test('check voting power', async () => {
-      await getWithAttempts(
-        neutronChain.blockWaiter,
-        async () => await dao.queryTotalVotingPower(),
-        async (response) => response.power == 11000,
-        20,
-      );
-    });
-  });
+  describe('Timelock: Succeed execution', () => {
+    let proposalId: number;
+    beforeAll(async () => {
+      const coinsForDemo2 = 2000;
+      proposalId = await subdaoMember1.submitSendProposal('send', 'send', [
+        {
+          recipient: neutronAccount2.wallet.address.toString(),
+          amount: coinsForDemo2,
+          denom: NEUTRON_DENOM,
+        },
+      ]);
 
-  describe('prepare: bond funds', () => {
-    test('bond form wallet 1', async () => {
-      await daoMember1.bondFunds('10000');
-      await getWithAttempts(
-        neutronChain.blockWaiter,
-        async () =>
-          await dao.queryVotingPower(daoMember1.user.wallet.address.toString()),
-        async (response) => response.power == 10000,
-        20,
+      const timelockedProp = await subdaoMember1.supportAndExecuteProposal(
+        proposalId,
       );
-    });
-    test('check voting power', async () => {
-      await getWithAttempts(
-        neutronChain.blockWaiter,
-        async () => await dao.queryTotalVotingPower(),
-        async (response) => response.power == 11000,
-        20,
-      );
-    });
-  });
 
-  describe('Cron params proposal', () => {
-    test('create proposal', async () => {
-      const chainManagerAddress = (await neutronChain.getChainAdmins())[0];
-      await daoMember1.submitUpdateParamsCronProposal(
-        chainManagerAddress,
-        'Proposal #1',
-        'Cron update params proposal. Will pass',
-        updateCronParamsProposal({
-          security_address: dao.contracts.voting.address,
-          limit: 10,
-        }),
-        '1000',
-      );
+      expect(timelockedProp.id).toEqual(proposalId);
+      expect(timelockedProp.status).toEqual('timelocked');
+      expect(timelockedProp.msgs).toHaveLength(1);
     });
 
-    describe('vote for proposal', () => {
-      const proposalId = 5;
-      test('vote YES from wallet 1', async () => {
-        await daoMember1.voteYes(proposalId);
-      });
-    });
+    test('execute timelocked: success', async () => {
+      await neutronAccount1.msgSend(subDao.contracts.core.address, '20000'); // fund the subdao treasury
+      const balance2 = await neutronAccount2.queryDenomBalance(NEUTRON_DENOM);
+      await waitSeconds(20);
+      await subdaoMember1.executeTimelockedProposal(proposalId);
+      const balance2After = await neutronAccount2.queryDenomBalance(
+        NEUTRON_DENOM,
+      );
+      expect(balance2After).toEqual(balance2 + 2000);
 
-    describe('execute proposal', () => {
-      const proposalId = 1;
-      let paramsBefore: { params: any };
-      test('check if proposal is passed', async () => {
-        await dao.checkPassedProposal(proposalId);
-      });
-      test('execute passed proposal', async () => {
-        paramsBefore = await neutronChain.queryCronParams();
-        await daoMember1.executeProposalWithAttempts(proposalId);
-      });
-      test('check if params changed after proposal execution', async () => {
-        const paramsAfter = await neutronChain.queryCronParams();
-        expect(paramsAfter.params.security_address).not.toEqual(
-          paramsBefore.params.security_address,
-        );
-        expect(paramsAfter.params.security_address).toEqual(
-          dao.contracts.voting.address,
-        );
-      });
+      const timelockedProp = await subDao.getTimelockedProposal(proposalId);
+      expect(timelockedProp.id).toEqual(proposalId);
+      expect(timelockedProp.status).toEqual('executed');
+      expect(timelockedProp.msgs).toHaveLength(1);
     });
   });
 });
