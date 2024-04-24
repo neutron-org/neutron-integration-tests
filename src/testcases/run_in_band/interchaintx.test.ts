c3a0870cde7e7160bce2a0307c047fe9f52273cd
nhpd <dmitry.klpv@gmail.com>
2024-04-24 14:04:30 +0400

    fix lint

--- src/testcases/run_in_band/interchaintx.test.ts
+++ src/testcases/run_in_band/interchaintx.test.ts
@@ -202,7 +202,7 @@ describe('Neutron / Interchain TXs', () => {
           icaId1,
           sequenceId,
         );
-        const ares = await getAcks(neutronChain, contractAddress);
+        await getAcks(neutronChain, contractAddress);
         expect(qres).toMatchObject<AcknowledgementResult>({
           success: ['/cosmos.staking.v1beta1.MsgDelegateResponse'],
         });
