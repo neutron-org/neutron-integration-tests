1ab453781ba98bb1ee8657c483d5b96926a71b13
quasisamurai <quazisamurai@gmail.com>
2024-08-08 17:11:11 -0300

    use old querier

--- src/testcases/run_in_band/interchain_tx_query_plain.test.ts
+++ src/testcases/run_in_band/interchain_tx_query_plain.test.ts
@@ -19,7 +19,7 @@ import {
   COSMOS_DENOM,
   NEUTRON_DENOM,
 } from '../../helpers/constants';
-import { QueryClientImpl as BankQuerier } from '@neutron-org/neutronjs/cosmos/bank/v1beta1/query.rpc.Query';
+import { QueryClientImpl as BankQuerier } from 'cosmjs-types/cosmos/bank/v1beta1/query';
 
 import config from '../../config.json';
 import { Wallet } from '../../helpers/wallet';
