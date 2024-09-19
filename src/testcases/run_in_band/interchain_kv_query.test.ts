76a34b95c029591c3db9dfdc4156235509270548
Aleksandr Pismenskiy <aleksandr.pismenski@hadronlabs.org>
2024-09-19 15:29:08 +0300

    fix test

--- src/testcases/run_in_band/interchain_kv_query.test.ts
+++ src/testcases/run_in_band/interchain_kv_query.test.ts
@@ -353,13 +353,11 @@ describe('Neutron / Interchain KV Query', () => {
       });
 
       test('register icq #7: balance', async () => {
-        const updatePeriod = (await neutronClient.getHeight()) + 1_000_000;
-
         await registerBalancesQuery(
           neutronClient,
           contractAddress,
           connectionId,
-          updatePeriod,
+          1_000_000,
           [COSMOS_DENOM],
           testState.wallets.cosmos.val1.address,
         );
@@ -860,7 +858,7 @@ describe('Neutron / Interchain KV Query', () => {
           async (response) =>
             response.registered_query.last_submitted_result_local_height > 0 &&
             response.registered_query.last_submitted_result_local_height + 5 <
-              (await neutronClient.getHeight()),
+            (await neutronClient.getHeight()),
           20,
         );
 
@@ -887,7 +885,7 @@ describe('Neutron / Interchain KV Query', () => {
             return (
               balances[0].denom === beforeBalances[0].denom &&
               parseInt(balances[0].amount || '0') >
-                parseInt(beforeBalances[0].amount || '0')
+              parseInt(beforeBalances[0].amount || '0')
             );
           },
 
