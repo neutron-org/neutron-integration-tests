87702ca1d0874a9eb5946bcf3cbbbf89d1f355ee
quasisamurai <quazisamurai@gmail.com>
2024-09-30 22:40:43 -0300

    checkot from main

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -75,6 +75,7 @@ export const CONTRACTS = {
   LOCKDROP_VAULT_CL: 'lockdrop_vault_for_cl_pools.wasm',
   MARKETMAP: 'marketmap.wasm',
   ORACLE: 'oracle.wasm',
+  IBC_RATE_LIMITER: 'rate_limiter.wasm',
 };
 
 export const NEUTRON_PREFIX = process.env.NEUTRON_ADDRESS_PREFIX || 'neutron';
