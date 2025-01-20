4d71c3802f7a8514ab14ff8621869e0d4a523e0c
pr0n00gler <programmer10110@gmail.com>
2025-01-20 16:09:59 +0200

    fix tests

--- src/testcases/run_in_band/tokenomics.test.ts
+++ src/testcases/run_in_band/tokenomics.test.ts
@@ -1,5 +1,9 @@
 import '@neutron-org/neutronjsplus';
-import { COSMOS_DENOM, NEUTRON_DENOM } from '../../helpers/constants';
+import {
+  COSMOS_DENOM,
+  IBC_ATOM_DENOM,
+  NEUTRON_DENOM,
+} from '../../helpers/constants';
 import { inject } from 'vitest';
 import { LocalState } from '../../helpers/local_state';
 import { QueryClientImpl as FeeburnerQueryClient } from '@neutron-org/neutronjs/neutron/feeburner/query.rpc.Query';
@@ -12,6 +16,15 @@ import { SigningNeutronClient } from '../../helpers/signing_neutron_client';
 import { MsgTransfer } from '@neutron-org/neutronjs/ibc/applications/transfer/v1/tx';
 import { Wallet } from '../../helpers/wallet';
 import config from '../../config.json';
+import { QueryClientImpl as FeemarketQueryClient } from '@neutron-org/neutronjs/feemarket/feemarket/v1/query.rpc.Query';
+import {
+  Dao,
+  DaoMember,
+  getDaoContracts,
+  getNeutronDAOCore,
+} from '@neutron-org/neutronjsplus/dist/dao';
+import { QueryClientImpl as AdminQueryClient } from '@neutron-org/neutronjs/cosmos/adminmodule/adminmodule/query.rpc.Query';
+import { DynamicFeesParams } from '@neutron-org/neutronjsplus/dist/proposal';
 
 describe('Neutron / Tokenomics', () => {
   let testState: LocalState;
@@ -23,6 +36,10 @@ describe('Neutron / Tokenomics', () => {
 
   let bankQuerier: BankQueryClient;
   let feeburnerQuerier: FeeburnerQueryClient;
+  let mainDao: Dao;
+  let chainManagerAddress: string;
+  let feemarketQuerier: FeemarketQueryClient;
+  let daoMember: DaoMember;
 
   beforeAll(async () => {
     testState = await LocalState.create(config, inject('mnemonics'));
@@ -45,11 +62,112 @@ describe('Neutron / Tokenomics', () => {
     treasuryContractAddress = (await feeburnerQuery.params()).params
       .treasuryAddress;
 
+    const daoCoreAddress = await getNeutronDAOCore(
+      neutronClient,
+      neutronRpcClient,
+    );
+    const daoContracts = await getDaoContracts(neutronClient, daoCoreAddress);
+    mainDao = new Dao(neutronClient, daoContracts);
+    daoMember = new DaoMember(
+      mainDao,
+      neutronClient.client,
+      neutronWallet.address,
+      NEUTRON_DENOM,
+    );
+    await daoMember.bondFunds('10000');
+    await neutronClient.getWithAttempts(
+      async () => await mainDao.queryVotingPower(daoMember.user),
+      async (response) => response.power >= 10000,
+      20,
+    );
+
     bankQuerier = new BankQueryClient(neutronRpcClient);
     feeburnerQuerier = new FeeburnerQueryClient(neutronRpcClient);
+
+    feemarketQuerier = new FeemarketQueryClient(neutronRpcClient);
+    const adminQuery = new AdminQueryClient(neutronRpcClient);
+    const admins = await adminQuery.admins();
+    chainManagerAddress = admins.admins[0];
+
+    await executeSwitchFeemarket(
+      feemarketQuerier,
+      daoMember,
+      'enable feemarket',
+      true,
+    );
+
+    await executeChangeGasPrices(daoMember, 'dynamicfees gasprices', {
+      ntrn_prices: [
+        {
+          denom:
+            'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2',
+          amount: '5',
+        },
+      ],
+    });
   });
 
-  describe('75% of Neutron fees are burned', () => {
+  const executeSwitchFeemarket = async (
+    feemarketQuery: FeemarketQueryClient,
+    daoMember: DaoMember,
+    kind: string,
+    enabled: boolean,
+    window = 1n,
+  ) => {
+    const params = (await feemarketQuery.params()).params;
+    params.enabled = enabled;
+    params.window = window;
+
+    const proposalId = await daoMember.submitFeeMarketChangeParamsProposal(
+      chainManagerAddress,
+      'Change Proposal - ' + kind + ' #',
+      'Param change proposal. It will change enabled params of feemarket module.',
+      '1000',
+      {
+        alpha: params.alpha,
+        beta: params.beta,
+        delta: params.delta,
+        min_base_gas_price: params.minBaseGasPrice,
+        min_learning_rate: params.minLearningRate,
+        max_learning_rate: params.maxLearningRate,
+        max_block_utilization: Number(params.maxBlockUtilization),
+        window: Number(params.window),
+        fee_denom: params.feeDenom,
+        enabled: params.enabled,
+        distribute_fees: params.distributeFees,
+      },
+    );
+
+    await daoMember.voteYes(proposalId, 'single', {
+      gas: '4000000',
+      amount: [{ denom: NEUTRON_DENOM, amount: '100000' }],
+    });
+    await mainDao.checkPassedProposal(proposalId);
+    await daoMember.executeProposalWithAttempts(proposalId);
+  };
+
+  const executeChangeGasPrices = async (
+    daoMember: DaoMember,
+    kind: string,
+    params: DynamicFeesParams,
+  ) => {
+    const proposalId = await daoMember.submitDynamicfeesChangeParamsProposal(
+      chainManagerAddress,
+      'Change Proposal - ' + kind + ' #',
+      'Param change proposal. It will change gas price list of dynamicfees/feemarket module.',
+      '1000',
+      params,
+    );
+
+    await daoMember.voteYes(proposalId, 'single', {
+      gas: '4000000',
+      amount: [{ denom: NEUTRON_DENOM, amount: '100000' }],
+    });
+    await mainDao.checkPassedProposal(proposalId);
+    await daoMember.executeProposalWithAttempts(proposalId);
+  };
+
+  describe('100% of Neutron fees are burned', () => {
     const bigFee = {
       gas: '200000',
       amount: [{ denom: NEUTRON_DENOM, amount: (10e8).toString() }],
@@ -74,11 +192,13 @@ describe('Neutron / Tokenomics', () => {
     });
 
     test('Total burned neutrons amount has increased', async () => {
+      const params = await feemarketQuerier.params();
+      console.log('KEKEKEKE', params.params);
       const burnedAfter = await feeburnerQuerier.totalBurnedNeutronsAmount();
       const diff =
         +(burnedAfter.totalBurnedNeutronsAmount.coin.amount || 0) -
         +(burnedBefore.totalBurnedNeutronsAmount.coin.amount || 0);
-      expect(diff).toBeGreaterThanOrEqual(10e8 * 0.75);
+      expect(diff).toBeGreaterThanOrEqual(10e8);
     });
   });
 
